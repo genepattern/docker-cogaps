839663adf28a109d681a5f4fe279249003926173
Tom Sherman <tomsherman159@gmail.com>
2018-08-23 21:20:11 -0400

    cleaned up arg parsing, don't pass files directly for now

--- src/run_cogaps_module.R
+++ src/run_cogaps_module.R
@@ -32,18 +32,20 @@ print(packageVersion("CoGAPS"))
 option_list <- list(
   # Note: it's not necessary for the names to match here, it's just a convention
   # to keep things consistent.
-  make_option("--num.iterations", type="integer", dest="num.iterations"),
-  make_option("--seed", type="integer", dest="seed"),
-  make_option("--distributed.method", dest="distributed.method"), # should be either NULL, "genome-wide", "single-cell"
-  make_option("--num.sets", type="integer", dest="num.sets"),
   make_option("--input.file", dest="input.file"),
-  make_option("--stddev.input.file", dest="stddev.input.file"),
-  #make_option("--stddev.decimal.value", type="double", dest="stddev.decimal.value"),
+  make_option("--transpose.data", type="logical", dest="transpose.data"),
   make_option("--pattern.start", type="integer", dest="pattern.start"),
   make_option("--pattern.stop", type="integer", dest="pattern.stop"),
   make_option("--pattern.step", type="integer", dest="pattern.step"),
+  make_option("--num.iterations", type="integer", dest="num.iterations"),
+  make_option("--seed", type="integer", dest="seed"),
+  make_option("--single.cell", type="logical", dest="single.cell"),
+  make_option("--distributed.method", dest="distributed.method"), # should be either "none", "genome-wide", "single-cell"
+  make_option("--num.sets", type="integer", dest="num.sets"),
+  make_option("--num.threads", type="integer", dest="num.threads"),
+  make_option("--stddev.input.file", dest="stddev.input.file"),
   make_option("--output.file", dest="output.file")
-  )
+)
 
 # Parse the command line arguments with the option list, printing the result
 # to give a record as with sessionInfo.
@@ -60,54 +62,94 @@ print(packageVersion("CoGAPS"))
 # Note that since some parameters are optional, we must be prepared to receive no value
 # Also check for blank values since these are ignored.
 if (is.null(opts$stddev.input.file) || grepl("^[[:space:]]*$", opts$stddev.input.file)) {
-   stddev.input.file <- NULL
+   stddev_input <- NULL
 } else {
-   stddev.input.file <- opts$stddev.input.file
+    if (!file.exists(opts$stddev.input.file)) {
+        stop("StdDev GCT file does not exist")
+    }
+    stddev_gct <- read.gct(opts$stddev.input.file)
+    stddev_input <- as.matrix(stddev_gct[["data"]])
+    #stddev_input <- opts$stddev.input.file
 }
 
 # Optparse will validate increment.value and convert it to a numeric value or give it the
 # default value of 10 if missing.  We must check for NA however (and NULL, to be safe) as
 # it will use that for non-numeric values.
+
+# Create the CogapsParams object - read the parameters into this
+params <- new("CogapsParams")
+
+# parse nIterations
 if (is.null(opts$num.iterations) || is.na(opts$num.iterations)) {
    stop("Parameter num.iterations must be numeric")
-} else {
-   num.iterations <- strtoi(opts$num.iterations)
 }
+params <- setParam(params, "nIterations", opts$num.iterations)
 
-if (is.null(opts$num.sets) || is.na(opts$num.sets)) {
-   stop("Parameter num.sets must be numeric")
-} else {
-   num.sets <- strtoi(opts$num.sets)
-}
-
+# parse seed
 if (is.null(opts$seed) || is.na(opts$seed)) {
    stop("Parameter seed must be numeric")
-} else {
-   seed <- opts$seed
+}
+params <- setParam(params, "seed", opts$seed)
+
+# parse singleCell
+if (is.null(opts$single.cell) || is.na(opts$single.cell)) {
+   stop("Parameter single.cell must be logical")
+}
+params <- setParam(params, "singleCell", opts$single.cell)
+
+# parse distributed
+if (is.null(opts$distributed.method) || is.na(opts$distributed.method) || !(opts$distributed.method %in% c("none", "genome-wide", "single-cell"))) {
+    stop("distributed.method must be either none, genome-wide, or single-cell")
+}
+if (opts$distributed.method != "none") {
+  params <- setParam(params, "distributed", opts$distributed.method)
 }
 
-prange <- paste("seq(", opts$pattern.start, ", ", opts$pattern.stop, ", by=", opts$pattern.step, ")", sep="")
-patternRange <- eval(parse(text=prange))
+# parse nSets
+if (is.null(opts$num.sets) || is.na(opts$num.sets)) {
+   stop("Parameter num.sets must be numeric")
+}
+params <- setDistributedParams(params, nSets=opts$num.sets)
+
+# these arguments are passed directly
+if (is.null(opts$transpose.data) || is.na(opts$transpose.data)) {
+   stop("Parameter transpose.data must be logial")
+}
+if (is.null(opts$num.threads) || is.na(opts$num.threads)) {
+   stop("Parameter num.threads must be numeric")
+}
+
+# create pattern range
+#prange <- paste("seq(", opts$pattern.start, ", ", opts$pattern.stop, ", by=", opts$pattern.step, ")", sep="")
+#patternRange <- eval(parse(text=prange))
+if (is.null(opts$pattern.start) || is.na(opts$pattern.start)) {
+    stop("Parameter pattern.start must be numeric")
+}
+if (is.null(opts$pattern.stop) || is.na(opts$pattern.stop)) {
+    stop("Parameter pattern.stop must be numeric")
+}
+if (is.null(opts$pattern.step) || is.na(opts$pattern.step)) {
+    stop("Parameter pattern.step must be numeric")
+}
+patternRange <- seq(opts$pattern.start, opts$pattern.stop, opts$pattern.step)
 
 # Load the GCT input file.
 print("Loading gct file now")
-if (!file.exists(opts$input.file)){
-     print("GCT file does not exist")
+if (!file.exists(opts$input.file)) {
+    stop("GCT file does not exist")
 }
+gct <- read.gct(opts$input.file)
+data_input <- as.matrix(gct[["data"]])
+# data_input <- opts$input.file
 
-# R object that manage CoGAPS parameters
-params <- new("CogapsParams")
-params <- setParam(params, "nIterations", num.iterations)
-params <- setParam(params, "seed", seed)
-params <- setParam(params, "distributed", opts$distributed.method)
-params <- setDistributedParams(params, num.sets)
-
+# loop over patternRange
 files2zip = {} 
 cogapsResult <- list()
 for (nPatterns in patternRange)
 {
-    cogapsResult[[nPatterns]] <- CoGAPS(data=opts$input.file,
-        params=params, uncertainty=stddev.input.file)
+    cogapsResult[[nPatterns]] <- CoGAPS(data=data_input, params=params,
+        nPatterns=nPatterns, transposeData=opts$transpose.data,
+        nThreads=opts$num.threads, uncertainty=stddev_input)
   
     currentResult <- cogapsResult[[nPatterns]]
     
@@ -115,8 +157,6 @@ for (nPatterns in patternRange)
     # dirName = paste("n_", nPatterns, sep="")
     # dir.create(dirName)
     dirName="."
-
-
 
     pdf(file.path(dirName, paste(opts$output.file,"_", nPatterns, ".pdf", sep="")))
     plot(currentResult)
--- test/cogaps/testRscript.sh
+++ test/cogaps/testRscript.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-Rscript --no-save --quiet --slave --no-restore ../../src/run_cogaps_module.R --input.file=data/all_aml_test.gct  --num.iterations=1000 --num.sets=4  --pattern.start=2 --pattern.stop=4 --pattern.step=1  --seed=123   --output.file=testLocalOutputs
+Rscript --no-save --quiet --slave --no-restore ../../src/run_cogaps_module.R \
+    --input.file=data/all_aml_test.gct \
+    --transpose.data=false \
+    --pattern.start=2 \
+    --pattern.stop=4 \
+    --pattern.step=1 \
+    --num.iterations=1000 \
+    --seed=123 \
+    --single.cell=false \
+    --distributed.method=none \
+    --num.sets=4 \
+    --num.threads=1 \
+    --output.file=testLocalOutputs
