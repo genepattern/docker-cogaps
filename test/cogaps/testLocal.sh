528f7bd9153be7adfa041bcac16a26ee19deaa66
ted Liefeld <liefeld@broadinstitute.org>
2019-02-25 15:49:44 -0500

    re-aligning manifest and paramgroups.json for new parameter settings

--- test/cogaps/testLocal.sh
+++ test/cogaps/testLocal.sh
@@ -2,10 +2,15 @@
 
 LOCAL_DIR=$PWD
 
-COMMAND_LINE="Rscript --no-save --quiet --slave --no-restore  /usr/local/bin/cogaps/run_cogaps_module.R   --input.file=$LOCAL_DIR/data/all_aml_test.gct --num.threads=4 --single.cell= FALSE --distributed.method=none --transpose.data=FALSE  --stddev.decimal.value=0.1   --num.iterations=1000  --pattern.start=2 --pattern.stop=4 --pattern.step=1  --seed=123   --output.file=testLocalOutputs --num.sets=5 "
+#COMMAND_LINE="Rscript --no-save --quiet --slave --no-restore  /usr/local/bin/cogaps/run_cogaps_module.R   --input.file=$LOCAL_DIR/data/all_aml_test.gct --param.file=$LOCAL_DIR/data/default_cogaps_params.rds --num.threads=4 --single.cell= FALSE --distributed.method=none --transpose.data=FALSE  --stddev.decimal.value=0.1   --num.iterations=1000  --pattern.start=2 --pattern.stop=4 --pattern.step=1  --seed=123   --output.file=testLocalOutputs --num.sets=5 "
+
+#COMMAND_LINE="Rscript --no-save --quiet --slave --no-restore  /usr/local/bin/cogaps/run_cogaps_module.R   --data.file=$LOCAL_DIR/data/all_aml_test.gct --param.file=$LOCAL_DIR/data/default_cogaps_params.rds --n.threads=4 --transpose.data=FALSE --output.file=testLocalOutputs"  
+
+COMMAND_LINE="Rscript --no-save --quiet --slave --no-restore  /usr/local/bin/cogaps/run_cogaps_module.R   --data.file=$LOCAL_DIR/data/all_aml_test.gct --param.file=$LOCAL_DIR/data/default_cogaps_params.rds --n.patterns=10 --n.threads=4 --transpose.data=FALSE --output.file=testLocalOutputs"  
 
 
-docker run -v $LOCAL_DIR:$LOCAL_DIR -w $LOCAL_DIR/Job_3113 -t genepattern/docker-cogaps:v0.4 $COMMAND_LINE
+echo $COMMAND_LINE
+docker run -v $LOCAL_DIR:$LOCAL_DIR -w $LOCAL_DIR/Job_3113 -t genepattern/docker-cogaps:v0.6a $COMMAND_LINE
 
 
 
